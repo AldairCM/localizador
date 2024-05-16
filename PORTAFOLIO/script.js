b33d04ffe8556769f8d3c4ecdadf5d9a1db05e8f
AldairCM <zabdynomercy@gmail.com>
2024-05-16 00:37:09 -0600

    Delete PORTAFOLIO directory

--- PORTAFOLIO/script.js
+++
@@ -1,7 +0,0 @@
-function openPDF(pdfName) {
-    // Concatenar la ruta del archivo PDF local
-    var localFilePath = 'pdfs/' + pdfName;
-    
-    // Abrir el PDF usando window.open
-    window.open(localFilePath, '_blank', 'location=no');
-}
